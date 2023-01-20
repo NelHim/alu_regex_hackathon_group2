efc6d0528e09eafe9b65eb7e54451673d22d5622
Nelson Himbaza <n.himbaza@alustudent.com>
2023-01-20 22:31:08 +0200

    Update jokesPattern_Nelson.py

--- jokesPattern_Nelson.py
+++ jokesPattern_Nelson.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
 
+import re
+
+joke_pattern = re.compile(r'^Why did the (.*)\? Because (.*)$')
+
+match = joke_pattern.search("Why did the banana go to the doctor? Because it wasn't peeling well.")
+
+if match:
+    print(match.group(1)) # banana go to the doctor
+    print(match.group(2)) # it wasn't peeling well
+else:
+    print("Invalid pattern")
